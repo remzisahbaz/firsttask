25db9ef61219e993cdf3186b7ddda4e7746955cb
RShbz <remzisahbaz@outlook.com>
2021-02-13 10:44:51 +0300

    firsttask

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {makeStyles} from "@material-ui/core/styles"
+import PenIcon from "@material-ui/icons/Create"
+import {
+  CssBaseline,
+  Container,
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+}from "@material-ui/core"
 
-function App() {
+const useStyles= makeStyles(theme=>({
+  root:{
+    flexGrow:1,
+
+  },
+  menuButton:{
+    marginRight: theme.spacing(2)
+  },
+  title:{
+    flexGrow:1,
+  },
+  container:{marginTop:theme.spacing(3)},
+
+}));
+
+
+const App = () => {
+  const classes= useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+          <>
+            <CssBaseline></CssBaseline>
+            <Container maxWidth="lg">
+              <AppBar position="static"  color="inherit" elevation={0}>
+                <Toolbar>
+                  <IconButton edge="start" className={classes.container} color="inherit"/>
+                  <Typography variant="h6" color="secondary" calassName={classes.title}>
+                    <a href="http://localhost:3000/post">First Task</a>
+                  </Typography>
+                  <Button color="primary" variant="outlined" startIcon={<PenIcon/>}>
+                    Yeni Proje
+                  </Button>
+                </Toolbar>
+              </AppBar>
+            </Container>
+          </>
+
+
+
+  )
 }
 
-export default App;
+export default App
---
+++ project/client/src/reducers/post.js
@@ -0,0 +1,23 @@
+import * as types from  "../actions/types";
+
+
+const initialState={
+    posts:[],
+};
+
+const postReducer=(state=initialState,action)=>
+{switch(action.type){
+    case types.FETCH_POSTS:
+        return {
+            ...state,
+            posts: action.payload,
+
+        };
+        
+        default:
+        return{...state,};
+}
+
+
+};
+export default postReducer;
